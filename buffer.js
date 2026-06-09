964cd24668faf096d26f0808e446a7dc75a0e4e1
Mahbub <mahbub2003814@gmail.com>
2026-06-09 12:11:45 +0600

    node js buffer day 2

--- buffer.js
+++ buffer.js
@@ -12,3 +12,22 @@ buffer[5] = 44; // ASCII for ','
 buffer[6] = 32; // ASCII for space
 buffer.write('Node', 7);
 console.log(buffer.toString());
+
+
+// buffer copy now :
+
+const source = Buffer.from('Hello, World!');
+const target = Buffer.alloc(source.length);
+
+console.log("target length : ", target)
+source.copy(target)
+console.log("copy : ", target)
+
+const partialText = Buffer.alloc(5)
+console.log("partialText : ", partialText)
+
+source.copy(partialText, 0, 7)
+
+console.log("Partial TEXT copy :",partialText)
+
+console.log(partialText.toString())
